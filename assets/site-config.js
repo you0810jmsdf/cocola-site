fe3cc1ee23216120c0d9ef2a7d46ca9f517db823
you0810jmsdf <you0810jmsdf@gmail.com>
2026-04-21 21:10:22 +0900

    Fix Instagram WebView: events calendar static page + GitHub Actions

--- assets/site-config.js
+++ assets/site-config.js
@@ -3,6 +3,6 @@ window.COCOLA_APP_LINKS = {
   creatorsMap: 'https://you0810jmsdf.github.io/creators-map/',
   heritageMap: 'https://script.google.com/macros/s/AKfycbyDcSt83tvCPYdBDvq4NJr5M16zSWW6k16VFoHF6cGzSOA4VhWDaIDKqCjSWuSpJZe2/exec',
   odekakeMap: 'https://you0810jmsdf.github.io/odekake-map/',
-  eventsList: 'https://script.google.com/macros/s/AKfycbycw2FBvcm34mS9R8SUNg28KscGqqGjR1zjz3hnLYq8ckgKCZObYaC9kbMs8R2SGXgC/exec?page=inzai',
+  eventsList: 'https://you0810jmsdf.github.io/cocola-site/events/',
   topicsEntryForm: 'https://docs.google.com/forms/d/1MXr-GdG5Jbu9aQE0xTR3xjWFl5XFkQXdl7asJNChMDQ/viewform'
 };
